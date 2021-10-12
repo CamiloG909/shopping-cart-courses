4c36f4d47abbf90ead5532543f0bb1edf4404a25
CamiloG <cgalindorivera@gmail.com>
2021-10-12 16:35:03 -0500

    new design

--- js/app.js
+++ js/app.js
@@ -1,86 +1,107 @@
-const cart = document.querySelector("#carrito");
-const containerCart = document.querySelector("#lista-carrito tbody");
-const clearCartBtn = document.querySelector("#vaciar-carrito");
-const listCourses = document.querySelector("#lista-cursos");
+const showCart = document.querySelector("#show-cart");
+const cart = document.querySelector(".cart");
+const containerCart = document.querySelector("#list-cart tbody");
+const clearCartBtn = document.querySelector("#clear-cart");
+const listCourses = document.querySelector("#list-courses");
 let arrayInfo = [];
 
 loadEventListeners();
+
 function loadEventListeners() {
-  listCourses.addEventListener("click", addCourse);
-  clearCartBtn.addEventListener("click", clearCartButton);
-  cart.addEventListener("click", deleteCourse);
+	showCart.addEventListener("mouseenter", showCartContainer);
+	showCart.addEventListener("mouseleave", hiddenCartContainer);
+	cart.addEventListener("mouseenter", showCartContainer);
+	cart.addEventListener("mouseleave", hiddenCartContainer);
+	showCart.addEventListener("click", showCartContainer);
+	showCart.addEventListener("click", hiddenCartContainer);
+	cart.addEventListener("click", showCartContainer);
+	cart.addEventListener("click", hiddenCartContainer);
+
+	listCourses.addEventListener("click", addCourse);
+	clearCartBtn.addEventListener("click", clearCartButton);
+	cart.addEventListener("click", deleteCourse);
+}
+
+function showCartContainer() {
+	cart.style.display = "block";
+	document.querySelector("#arrow-cart").style.display = "block";
+}
+
+function hiddenCartContainer() {
+	cart.style.display = "none";
+	document.querySelector("#arrow-cart").style.display = "none";
 }
 
 function addCourse(e) {
-  e.preventDefault();
+	e.preventDefault();
 
-  if (e.target.classList.contains("agregar-carrito")) {
-    const courseCard = e.target.parentElement.parentElement;
-    getInfoCourse(courseCard);
-  }
+	if (e.target.classList.contains("agregar-carrito")) {
+		const courseCard = e.target.parentElement.parentElement;
+		getInfoCourse(courseCard);
+	}
 }
 
 function getInfoCourse(card) {
-  const infoCourse = {
-    id: card.querySelector("a").getAttribute("data-id"),
-    img: card.querySelector("img").src,
-    title: card.querySelector(".info-card h4").textContent,
-    price: card.querySelector(".info-card .precio span").textContent,
-    amount: 1,
-  };
+	const infoCourse = {
+		id: card.querySelector("a").getAttribute("data-id"),
+		img: card.querySelector("img").src,
+		title: card.querySelector(".info-card h4").textContent,
+		price: card.querySelector(".info-card .precio span").textContent,
+		amount: 1,
+	};
 
-  const exists = arrayInfo.some((e) => e.id === infoCourse.id);
+	const exists = arrayInfo.some((e) => e.id === infoCourse.id);
 
-  if (exists) {
-    const curses = arrayInfo.map((e) => {
-      if (e.id === infoCourse.id) {
-        e.amount++;
-        return e;
-      } else {
-        return e;
-      }
-    });
-  } else {
-    arrayInfo.push(infoCourse);
-  }
+	if (exists) {
+		const curses = arrayInfo.map((e) => {
+			if (e.id === infoCourse.id) {
+				e.amount++;
+				return e;
+			} else {
+				return e;
+			}
+		});
+	} else {
+		arrayInfo.push(infoCourse);
+	}
 
-  showCourse();
+	showCourse();
 }
 
 function showCourse() {
-  clearCartContainer();
+	clearCartContainer();
 
-  arrayInfo.forEach((e) => {
-    const row = document.createElement("tr");
-    row.innerHTML = `
+	arrayInfo.forEach((e) => {
+		const row = document.createElement("tr");
+		row.innerHTML = `
 		<td><img src="${e.img}" width=100></td>
 		<td>${e.title}</td>
 		<td>${e.price}</td>
 		<td>${e.amount} </td>
 		<td>
-			<a href="#" class="borrar-curso" data-id="${e.id}">X</a>
+    <i class="bi bi-x-circle-fill cart__btn-delete" title="Delete course" data-id="${e.id}"></i>
 		</td>
 		`;
-    containerCart.appendChild(row);
-  });
+		containerCart.appendChild(row);
+	});
 }
 
 function clearCartContainer() {
-  containerCart.innerHTML = "";
+	containerCart.innerHTML = "";
 }
 
 function clearCartButton(e) {
-  e.preventDefault();
-  arrayInfo = [];
-  clearCartContainer();
+	e.preventDefault();
+	arrayInfo = [];
+	clearCartContainer();
 }
 
 function deleteCourse(e) {
-  e.preventDefault();
-  if (e.target.classList.contains("borrar-curso")) {
-    const idCourse = e.target.getAttribute("data-id");
+	e.preventDefault();
+	if (e.target.classList.contains("borrar-curso")) {
+		const idCourse = e.target.getAttribute("data-id");
 
-    arrayInfo = arrayInfo.filter((e) => e.id !== idCourse);
-    showCourse();
-  }
+		arrayInfo = arrayInfo.filter((e) => e.id !== idCourse);
+		showCourse();
+	}
 }
